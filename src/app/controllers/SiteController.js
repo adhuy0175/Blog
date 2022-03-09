bf94a1677ea3b1b62c14a7f85d24f4389612e566
huy tran <adhuy016@gmail.com>
2022-03-09 09:02:09 +0700

    Install prettier, lint-staged, husky

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,15 +1,14 @@
-const res = require("express/lib/response");
+const res = require('express/lib/response');
 
-class SiteController{
-
-    //[GET] /
-    index(req, res){
-        res.render('home');
-    }
-    //[Get] / Search
-    search(req, res){
-        res.render('search');
-    }
+class SiteController {
+//[GET] /
+index(req, res) {
+res.render('home');
+}
+//[Get] / Search
+search(req, res) {
+res.render('search');
+}
 }
 //create a instance of the opject
-module.exports = new SiteController;
+module.exports = new SiteController();
--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
-const { engine } = require("express-handlebars");
+const { engine } = require('express-handlebars');
 const port = 3000;
 const app = express();
 
-
 const route = require('./routes');
-app.use(express.static(path.join(path.join(__dirname,'public'))));
+app.use(express.static(path.join(path.join(__dirname, 'public'))));
 //HTTP logger
 //app.use(morgan('combined'));
 app.use(express.urlencoded());
 app.use(express.json());
 //template engine
-app.engine("hbs", engine({
-    extname : '.hbs'
-}));
+app.engine(
+'hbs',
+engine({
+extname: '.hbs',
+}),
+);
 
-app.set("view engine", "hbs");
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/resources/views'));
 //Route init
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+console.log(`Example app listening at http://localhost:${port}`);
+});
